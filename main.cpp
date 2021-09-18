65aeddff04726b5f7d4aa73678aa8869987b584d
Bruno Julião <2057387+brunojuliao@users.noreply.github.com>
2021-09-18 18:40:13 +0000

    solution

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
+	
+int main()
+{
+  char str0[6]="98765",str1[5]="abcd",str2[10]="efghijklm";
 
-int main(int argc, char *argv[]) {
-    printf("Bem vindo ao C!!!\n");
-    return 0;
+   printf("str0 = %s\t str1 = %s\t str2 = %s\n",str0,str1,str2);
+   strncpy(str1,"1234567",sizeof(str1));
+   printf("str0 = %s\t str2 = %s\n",str0,str2);
 }
