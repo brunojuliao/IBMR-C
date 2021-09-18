167c8f792ab058e04369acea7a7d9dbca2389881
Bruno Julião <2057387+brunojuliao@users.noreply.github.com>
2021-09-18 18:08:45 +0000

    Adding last sample from unit 1

--- main.cpp
+++ main.cpp
@@ -1,7 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
+#include <stdbool.h>
+#include <string.h>
+
+struct pessoa {
+    char nome[50];
+    char email[80];
+};
 
 int main(int argc, char *argv[]) {
-    printf("Bem vindo ao C!!!\n");
+    struct pessoa p1;
+
+    printf("\n=== Cadastro de Usuário ===\n");
+
+    printf("Nome : ");
+    scanf("%s%*c", p1.nome);
+
+    printf("Email : ");
+    //flush stdin para ler o teclado e fgets() para atribuir na string
+    fflush(stdin);
+    fgets(p1.email, 80, stdin);
+
+    printf("\n === IMPRIMINDO === \n");
+    printf("\n Nome : ");
+    //também temos o puts para imprimir a string
+    puts(p1.nome);
+    printf("\n Email : %s", p1.email);
+
+    getchar();
+
     return 0;
 }
